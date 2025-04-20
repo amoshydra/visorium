1d7aec20b4e5511dee109dae3e442086b9cf34e7
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-04-21 04:11:32 +0800

    fix: fix typo prod instead of default

--- src/client/hooks/useFileServerSocket.ts
+++ src/client/hooks/useFileServerSocket.ts
@@ -1,6 +1,6 @@
 const isDemo = import.meta.env.VITE_VISORIUM_MODE === "demo";
 
-const id = isDemo ? "demo" : "prod";
+const id = isDemo ? "demo" : "default";
 
 const { useFileServerSocket } = await import(`./useFileServerSocket/${id}.ts`);
 export { useFileServerSocket };
