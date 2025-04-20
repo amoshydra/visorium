9c943df3b7ffeba4d55f7a0fd29fab2a96257da5
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-04-20 23:43:09 +0800

    ci: setup continuous deployment pipeline

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_URL ?? undefined,
   plugins: [react()],
   root: join(__dirname, "src/client"),
   build: {
