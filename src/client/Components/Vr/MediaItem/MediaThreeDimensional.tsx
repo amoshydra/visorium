c85cb5afdeebc28f8cc489abf96906cac365b04b
amoshydra <8733840+amoshydra@users.noreply.github.com>
2025-04-28 01:01:55 +0800

    fix: apply smooth shading to loaded glb

--- src/client/Components/Vr/MediaItem/MediaThreeDimensional.tsx
+++ src/client/Components/Vr/MediaItem/MediaThreeDimensional.tsx
@@ -1,6 +1,6 @@
 import { useGLTF } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useMemo } from "react";
+import { Mesh } from "three";
 import { MediaMessage } from "../MediaMessage";
 import { MediaComponent } from "./interface";
 
@@ -20,17 +20,26 @@ const InternalMediaThreeDimensional: MediaComponent = ({
 }) => {
   const { scene } = useGLTF(file.src);
 
-  useFrame(() => {});
+  const smoothenScene = useMemo(() => {
+    // Apply smooth shading to the 3D model
+    // Note: This will mutate the original scene object
+    scene.traverse((child) => {
+      if (child instanceof Mesh) {
+        child.geometry.computeVertexNormals();
+        child.material.flatShading = false;
+      }
+    });
+    return scene;
+  }, [scene]);
 
   return (
     <>
       <primitive
-        object={scene}
+        object={smoothenScene}
         position={position}
         scale={scale}
         rotation={rotation}
       />
-      ;
       <directionalLight
         position={[0.5, 1.0, 4.4]}
         castShadow
